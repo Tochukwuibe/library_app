2537fbbd2eec93f7a4505bee2d6fc283239fc731
Tochukwu Ibe <tochukwu.claver@gmail.com>
2019-01-11 09:36:37 -0500

    configured server for spa

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ const { DATABASE, PORT } = require('./config/config');
 const users = require('./routes/users/index');
 const books = require('./routes/books/index.js');
 const reviews = require('./routes/reviews/index.js');
-const  auth  = require('./middleware/auth');
+const auth = require('./middleware/auth');
+const path = require('path');
 
 mongoose.Promise = global.Promise;
 
@@ -17,17 +18,35 @@ async function init() {
 
     app.use(bodyParser.json());
     app.use(cookieParser());
-    
+
     app.use('/api/users', users);
+    app.use(express.static('client/build'));
+
+
 
     // app.use(auth);
     app.use('/api/books', books);
     app.use('/api/reviews', reviews);
 
+
+    // Serve the Index html of the client
+    if (process.env.NODE_ENV === 'production') {
+        app.get('/*', (req, res) => {
+
+            res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+        })
+    }
+
+
+
+
     app.listen(PORT, (err) => {
         if (!err) return console.log(`server listening on ${PORT}`)
         console.log('an error occoured starting server ', err);
     })
+
+
+
 }
 
 function connect() {
